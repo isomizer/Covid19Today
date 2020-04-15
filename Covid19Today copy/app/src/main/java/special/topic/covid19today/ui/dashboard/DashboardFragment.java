6884851ff0fb9f2265ad24ddaed37483ef28d499
kittitouchfah <55575889+kittitouchfah@users.noreply.github.com>
2020-04-16 01:36:57 +0700

    add popup

--- Covid19Today copy/app/src/main/java/special/topic/covid19today/ui/dashboard/DashboardFragment.java
+++ Covid19Today copy/app/src/main/java/special/topic/covid19today/ui/dashboard/DashboardFragment.java
@@ -1,13 +1,18 @@
 package special.topic.covid19today.ui.dashboard;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.PopupMenu;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.MainThread;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -20,12 +25,15 @@ import special.topic.covid19today.R;
 
 public class DashboardFragment extends Fragment {
 
+    Button button;
+
     RadioGroup rg;
     Button btnTable,btnGraph;
     private FragmentManager fragmentManager;
     private FragmentTransaction fragmentTransaction;
 
     private DashboardViewModel dashboardViewModel;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -35,6 +43,32 @@ public class DashboardFragment extends Fragment {
         rg = root.findViewById(R.id.radios);
         btnTable = root.findViewById(R.id.radioButtonTable);
         btnGraph = root.findViewById(R.id.radioButtonGraph);
+        button = root.findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final PopupMenu popupMenu = new PopupMenu(getActivity() , button);
+                popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
+
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+
+
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(getActivity().getApplicationContext(), "" + item.getTitle(), Toast.LENGTH_SHORT).show();
+                        return true;
+
+                    }
+                });
+                popupMenu.show();
+
+            }
+        });
+
+
+
 
         btnGraph.setOnClickListener(new View.OnClickListener() {
 
