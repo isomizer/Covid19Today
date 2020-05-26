b34d91c6d1e3141196d7caab1cfcdaa7873c461a
Nonchai <32590129+nonchai000@users.noreply.github.com>
2020-05-26 20:01:28 +0700

    Open chatbot
    from assists\index.html

--- app/src/main/java/special/topic/covid19today/ui/chatbot/ChatbotFragment.java
+++ app/src/main/java/special/topic/covid19today/ui/chatbot/ChatbotFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -12,10 +15,33 @@ import special.topic.covid19today.R;
 
 public class ChatbotFragment extends Fragment {
 
+    protected WebView webView;
+    private static final String ENROLLMENT_URL = "file:///android_asset/index.html";
+    //private static final String ENROLLMENT_URL = "https://www.google.co.th/";
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_chatbot, container, false);
+
+        webView = root.findViewById(R.id.local_dialog_webview);
+        webView.setWebViewClient(new WebViewClient());
+
+        WebSettings webSettings = webView.getSettings();
+
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setLoadWithOverviewMode(true);
+
+        webSettings.setSupportZoom(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(false);
+
+        refreshWebView(webView);
+
         return root;
     }
 
+    public void refreshWebView(View view) {
+        webView.loadUrl(ENROLLMENT_URL);
+    }
 }
